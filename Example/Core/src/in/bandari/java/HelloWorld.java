79939a0224f209fa9d1379121d53c7ed5e026703
Hareesh505 <bandari.harish505@gmail.com>
2015-01-05 09:55:09 +0530

    Hello! World Program done

---
+++ Example/Core/src/in/bandari/java/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package in.bandari.java;
+
+/**
+ * @author Hareesh
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Hello! World");
+	}
+
+}
